ff71c6103a3f563c378201d43efddb0594a4f509
Shezad <shezadhassan22@gmail.com>
2021-08-07 16:18:24 +0800

    Changed styling and removed name

--- front/src/components/Notifications.js
+++ front/src/components/Notifications.js
@@ -1,19 +1,19 @@
-import React, {useContext} from 'react'
-import { SocketContext} from '../SocketContext';
+import React, { useContext } from "react";
+import { SocketContext } from "../SocketContext";
 
 const Notifications = () => {
-    const {answerCall, call, callAccepted} = useContext(SocketContext);
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
 
-    return (
-        <div className="notifications">
-        {call.isReceivedCall && !callAccepted && (
-            <div>
-                <h1>{call.name} is calling: </h1>
-                <button onClick={answerCall}>Answer</button>
-            </div>
-        )}
+  return (
+    <div className="notifications">
+      {call.isReceivedCall && !callAccepted && (
+        <div>
+          <h1> Receiving Call: </h1>
+          <button onClick={answerCall}>Answer</button>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Notifications;
--- front/src/components/Options.js
+++ front/src/components/Options.js
@@ -1,45 +1,48 @@
-import React, {useContext, useState} from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { SocketContext } from '../SocketContext';
+import React, { useContext, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { SocketContext } from "../SocketContext";
 
-const Options = ({children}) => {
-    const {me, callAccepted, name, setname, callEnded, leaveCall, callUser } = useContext(SocketContext);
-    const [idToCall, setidToCall] = useState('');
+const Options = ({ children }) => {
+  const { me, callAccepted, name, setname, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+  const [idToCall, setidToCall] = useState("");
 
-    function handleSubmit(e) {
-        e.preventDefault();
-    }
-    
-    return (
-        <div className="options">
-            <div className="account-info">
-                <form onSubmit={handleSubmit}>
-                    <input placeHolder="Enter Name" label="Name" value={name} onChange={(e) => {setname(e.target.value)}}/>
-                    {!callAccepted && !callEnded && (
-                    <CopyToClipboard text={me}>
-                        <button type="button">
-                        Copy Your ID
-                        </button>
-                    </CopyToClipboard>
-                    )}
-                </form>
-                
-            </div>
-            <div className="make-a-call">
-                <div>
-                    {callAccepted && !callEnded ? (
-                        <button onClick={leaveCall}>Hang Up</button>
-                    ) : (
-                        <form onSubmit={handleSubmit}>
-                            <input label="ID to Call" value={idToCall} onChange={(e) => setidToCall(e.target.value)} />
-                            <button type="button" onClick={() => callUser(idToCall)}>Call</button>
-                        </form>
-                    )}
-                </div>
-            </div>
-            {children}
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="options">
+      <div className="account-info">
+        <form onSubmit={handleSubmit}>
+          {!callAccepted && !callEnded && (
+            <CopyToClipboard text={me}>
+              <button type="button">Copy Your ID</button>
+            </CopyToClipboard>
+          )}
+        </form>
+      </div>
+      <div className="make-a-call">
+        <div>
+          {callAccepted && !callEnded ? (
+            <button onClick={leaveCall}>Hang Up</button>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <input
+                label="ID to Call"
+                value={idToCall}
+                onChange={(e) => setidToCall(e.target.value)}
+              />
+              <button type="button" onClick={() => callUser(idToCall)}>
+                Call
+              </button>
+            </form>
+          )}
         </div>
-    )
-}
+      </div>
+      {children}
+    </div>
+  );
+};
 
-export default Options
+export default Options;
